59f730a8f24aeafb66609124b09ec41a5064a6ed
Tomoaki Kawada <i@yvt.jp>
2015-09-20 01:04:40 +0900

    Raised spheremap precision to prevent glitch occuring when plane is nearly prependicular to the camera direction

--- src/renderer/shaders/SphereMap.glsl
+++ src/renderer/shaders/SphereMap.glsl
@@ -1,11 +1,11 @@
-vec2 encodeSpheremap(vec3 normal) {
-	vec2 e = normalize(normal.xy) * sqrt(normal.z * -0.5 + 0.5);
+highp vec2 encodeSpheremap(highp vec3 normal) {
+	highp vec2 e = normalize(normal.xy) * sqrt(normal.z * -0.5 + 0.5);
 	return e * 0.5 + 0.5;
 }
 
-vec3 decodeSpheremap(vec2 encoded) {
-	vec4 nn = vec4(encoded * 2. - 1., 1., -1.);
-	float l = dot(nn.xyz, -nn.xyw);
+highp vec3 decodeSpheremap(highp vec2 encoded) {
+	highp vec4 nn = vec4(encoded * 2. - 1., 1., -1.);
+	highp float l = dot(nn.xyz, -nn.xyw);
 	nn.z = l; nn.xy *= sqrt(l);
 	return nn.xyz * 2. + vec3(0., 0., -1.);
 }
