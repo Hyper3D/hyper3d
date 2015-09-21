7b983f4506c9c63d1ecbd2b42e036806e7b8d7f9
Tomoaki Kawada <i@yvt.jp>
2015-09-22 01:38:05 +0900

    Fixed LogRGB

--- src/renderer/shaders/LogRGB.glsl
+++ src/renderer/shaders/LogRGB.glsl
@@ -4,13 +4,13 @@
 vec4 encodeLogRGB(vec3 rgb)
 {
 	float lum = max(max(rgb.r, rgb.g), rgb.b);
-	float logval = min(max(lum, 1. / 4.), 16.);
+	float logval = min(floor(lum * 4. + 1.), 64.) * (1. / 4.);
 	float invLogval = 1. / logval;
-	return vec4(rgb * invLogval, (logval - 1./4.) * (4. / 63.));
+	return vec4(rgb * invLogval, logval * (1. / 16.));
 }
 
 vec3 decodeLogRGB(vec4 p)
 {
-	float t = p.a * 63. / 4. + 1. / 4.;
+	float t = floor(p.a * 64. + .5) * (1. / 4.);
 	return p.rgb * t;
 }
