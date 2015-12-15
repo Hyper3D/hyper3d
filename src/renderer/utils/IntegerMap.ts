d374799a4d15b50d7f2dcfc97026a75f0ff27b04
Tomoaki Kawada <i@yvt.jp>
2015-12-15 22:55:05 +0900

    Use ES6 Map

--- src/renderer/utils/IntegerMap.ts
+++ src/renderer/utils/IntegerMap.ts
@@ -3,92 +3,43 @@
 
 export class IntegerMap<T>
 {
-	private table: IntergerMapNode<T>[];
-	private first: IntergerMapNode<T>;
+	private map: Map<number, T>;
 	
 	constructor()
 	{
-		this.table = [];
-		this.first = null;
+		this.map = new Map<number, T>();
 	}
 	
 	get isEmpty(): boolean
 	{
-		return this.first == null;
+		return this.map.size == 0;
 	}
 	
 	get(key: number, defaultValue?: T): T
 	{
-		const node = this.table[key];
-		if (node) {
-			return node.value;
+		const ret = this.map.get(key);
+		if (typeof ret === 'undefined') {
+			return defaultValue;
 		}
-		return defaultValue;
+		return ret;
 	}
 	
 	set(key: number, value: T): T
 	{
-		let node = this.table[key];
-		if (node) {
-			node.value = value;
-		} else {
-			node = {
-				key, value,
-				prev: null,
-				next: null	
-			};
-			if (this.first) {
-				node.next = this.first;
-				node.prev = this.first.prev;
-				node.next.prev = node;
-				node.prev.next = node;
-			} else {
-				this.first = node.prev = node.next = node;
-			}
-			this.table[key] = node;
-		}
+		this.map.set(key, value);
 		return value;
 	}	
 	
 	remove(key: number): boolean
 	{
-		const node = this.table[key];
-		if (node) {
-			delete this.table[key];
-			if (this.first == node) {
-				if (node.next == node) {
-					this.first = null;
-				} else {
-					this.first = node.next;
-				}
-			}
-			if (node.next != node) {
-				node.next.prev = node.prev;
-				node.prev.next = node.next;
-			}
-			return true;
-		}
-		return false;
+		return this.map.delete(key);
 	}
 	
 	forEach(cb: (key: number, value: T) => void): void
 	{
-		if (this.first) {
-			let node = this.first;
-			do {
-				const next = node.next;
-				cb(node.key, node.value);
-				node = next;
-			} while (node != this.first && this.first);
-		}
+		this.map.forEach((value, key) => {
+			cb(key, value);
+		});
 	}
-}
-
-interface IntergerMapNode<T>
-{
-	key: number;
-	value: T;
-	prev: IntergerMapNode<T>;
-	next: IntergerMapNode<T>;
 }
 
