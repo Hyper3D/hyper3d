05dda7f3246744465bdea44f853dff74ddee2fb2
Tomoaki Kawada <i@yvt.jp>
2015-12-28 01:37:01 +0900

    Lighting stability fix

--- src/renderer/shaders/geometry/VS_ShadowMapGeometry.glsl
+++ src/renderer/shaders/geometry/VS_ShadowMapGeometry.glsl
@@ -10,5 +10,6 @@ void main()
     evaluateGeometry();
 
     gl_Position = u_viewProjectionMatrix * vec4(worldPosition, 1.);
+    gl_Position.z += 0.001 * gl_Position.w; // polygonOffset sometimes doesn't work well?
 }
 
