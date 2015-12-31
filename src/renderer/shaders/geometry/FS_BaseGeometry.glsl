acded1b5f0b3048f8e6f2560fc8631ed7484515d
Tomoaki Kawada <i@yvt.jp>
2015-12-31 20:41:53 +0900

    Changed default material

--- src/renderer/shaders/geometry/FS_BaseGeometry.glsl
+++ src/renderer/shaders/geometry/FS_BaseGeometry.glsl
@@ -18,7 +18,7 @@ void evaluateMaterial()
     m_albedo = vec3(1.);
     m_roughness = 0.2;
     m_metallic = 0.2;
-    m_specular = 0.;
+    m_specular = 0.03;
     m_normal = vec3(0., 0., 1.);
     m_emissive = vec3(0.);
     m_materialId = 0.;
