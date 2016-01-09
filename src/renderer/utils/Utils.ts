1386ed8590d62ca2cc7fd48064b6e8d84988bbf7
Tomoaki Kawada <i@yvt.jp>
2016-01-09 22:24:10 +0900

    Fixed ulog2

--- src/renderer/utils/Utils.ts
+++ src/renderer/utils/Utils.ts
@@ -7,19 +7,14 @@ export interface IDisposable
 const imul: (a: number, b: number) => number  = (<any> Math).imul;
 
 const deBruijnTable = [
-    0,  9,  1, 10, 13, 21,  2, 29, 11, 14, 16, 18, 22, 25,  3, 30,
-    8, 12, 20, 28, 15, 17, 24,  7, 19, 27, 23,  6, 26,  5,  4, 31
+    0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
+    31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9
 ];
 
 export const ulog2: (v: number) => number =
     imul ? (v: number) => {
         // have imul; use http://graphics.stanford.edu/~seander/bithacks.html#IntegerLogDeBruijn
-        v |= v >> 1;
-        v |= v >> 2;
-        v |= v >> 4;
-        v |= v >> 8;
-        v |= v >> 16;
-        return deBruijnTable[imul(v, 0x077cb531) >>> 27];
+        return deBruijnTable[imul(v, 0x077CB531) >>> 27];
     } : (v: number) => {
         let i = 0;
         while (v != 0) {
